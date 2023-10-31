9c77553b58d6f301b8980023da47c02ad64efb87
Kamen <genchev.kamen@gmail.com>
2023-10-31 17:30:47 +0200

    Refactor constants to be more clear

--- src/com/sirma/settings/ItemStatusConstants.java
+++ src/com/sirma/settings/ItemStatusConstants.java
@@ -1,6 +1,6 @@
 package com.sirma.settings;
 
 public class ItemStatusConstants {
-    public static final double BROKEN_DISCOUNT = 0.6;
-    public static final double PERISHED_DISCOUNT = 0.9;
+    public static final double BROKEN_DISCOUNT_PERCENTAGE = 0.6;
+    public static final double PERISHED_DISCOUNT_PERCENTAGE = 0.9;
 }
